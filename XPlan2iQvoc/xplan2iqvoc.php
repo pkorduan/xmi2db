947f8807fb56f69c17c46660b50e5567ef80045a
Peter Korduan <peter.korduan@gdi-service.de>
2016-04-22 11:11:10 +0200

    dev-5 Cleanup code.

--- XPlan2iQvoc/xplan2iqvoc.php
+++ XPlan2iQvoc/xplan2iqvoc.php
@@ -2,21 +2,25 @@
   include( dirname(__FILE__) . "/../conf/database_conf.php");
 
   $schema = $_REQUEST['schema'];
-  //echo $schema;
   
-  $full_model = "SELECT * FROM ".$schema.".full_model";
+  $full_model = "
+    SELECT
+      *
+    FROM" .
+      $schema . ".full_model
+  ";
   $result = pg_query($db_conn, $full_model);
   $current_par_package = '';
   $current_package = '';
   $current_class = '';
   $current_attribute = '';
+
   while ($row = pg_fetch_assoc($result)){
     $par_package = $row['parent_package_name'];
     $package = $row['package_name'];
     $class = $row['class_name'];
     $attribute = $row['atribute_name'];
-    if ($par_package==$current_par_package) ;
-    else {
+    if ($par_package != $current_par_package) {
       echo $par_package.";;;;;;;;<br>";
       $current_par_package=$par_package;
       if ($package!==$current_package) {
@@ -28,8 +32,6 @@
           }
         }
       }
-      
     }
-    //echo $row['parent_package_name']."<br>";
   }
 ?>
